71c0d2557ae4dd096814ce4ac46233c76af78e80
weltyc <chris.welty.1@gmail.com>
2014-02-16 01:32:13 -0600

    More tests and better docs for EngineeringFormat.

--- src/com/welty/othello/core/Engineering.java
+++ src/com/welty/othello/core/Engineering.java
@@ -3,25 +3,24 @@ package com.welty.othello.core;
 import com.orbanova.common.misc.Require;
 
 /**
- * Format numbers in Engineering format, for example "123k" for 123000.
- * <p/>
- * The converted value will have 1-3 digits to the left of the decimal place. It will be followed by
- * a space, then a single-character SI prefix denoting the power of 1000.
+ * Format longs or doubles in Engineering format, for example "123k" for 123000.
  * <p/>
  * There are three use cases:
  * <p/>
- * 1. Displaying a single long in a compact format. In this case, call {@link #formatLong(long)}
- * which will automatically choose an ldp and width.
+ * 1. Displaying a single long/double in a compact format. In this case, call {@link #compactFormat(long)}
+ * or {@link #compactFormat(double)} which will automatically choose a prefix and display the number with 3 significant
+ * figures (or fewer if it is a long between -99 and 99).
  * <p/>
- * 2. Displaying a set of longs in a column, expecting them to line up with a common SI prefix and decimal place in
+ * 2. Displaying a set of longs/doubles in a column, expecting them to line up with a common SI prefix and decimal place in
  * the same location (and therefore not expecting the smaller values to have as much precision, or necessarily any
- * precision at all). In this case, call {@link #formatLong(long, int, int, int)} with nPre, nPost, and prefix the same
- * for all values to be converted.
+ * precision at all). In this case, call {@link #formatLong(long, int, int, int)} or
+ * {@link #formatDouble(double, int, int, int)} with nPre, nPost, and prefix the same
+ * for all values to be converted, or the simpler {@link #formatLong(long, int)} or {@link #formatDouble(double, int)}
+ * which default 2 digits after the decimal and is padded to 4 characters before.
  * <p/>
  * 3. Displaying a set of longs in a column, expecting each to be displayed with the same number of decimal places
  * of precision (and therefore not expecting the suffixes to be the same or the decimal point to be in the same
- * place). In this case, call {@link #formatLong(long, int)}. In order to line things up, a missing SI prefix
- * is displayed as a space character in this case.
+ * place). This case is not implemented.
  */
 public class Engineering {
     private static char[] prefixes = " kMGTPE".toCharArray();
@@ -31,32 +30,139 @@ public class Engineering {
     // Engineering format - double
     ///////////////////////////////////////
 
-    public static String engineeringDouble(double x) {
-        return engineeringDouble(x, calcPrefix(x));
+    /**
+     * Format the double with exactly three decimal places of precision
+     *
+     * @param x value to format
+     * @return formatted double
+     */
+    public static String compactFormat(double x) {
+        final int nDecimalPlaces = 3;
+
+        if (x == 0) {
+            return "0.00 ";
+        }
+        final double absX = x < 0 ? -x : x;
+
+        int pow = (int) Math.floor(Math.log10(absX));
+        long abscissa = Math.round(absX * Math.pow(10, nDecimalPlaces - 1 - pow));
+        if (abscissa >= Math.pow(10, nDecimalPlaces)) {
+            abscissa /= 10;
+            pow++;
+        }
+
+        final int prefix = pow >= 0 ? pow / 3 : (pow - 2) / 3;
+        final int nPost = 2 - (pow - prefix * 3);
+        final StringBuilder sb = new StringBuilder();
+        if (prefix > 0) {
+            sb.append(prefixes[prefix]);
+        }
+        if (prefix < 0) {
+            sb.append(negPrefixes[-prefix]);
+        }
+        sb.append(' ');
+        appendShiftedLong(abscissa, sb, x < 0, 0, nPost);
+
+        return sb.reverse().toString();
+    }
+
+    public static String formatDouble(double x, int prefixIndex) {
+        return formatDouble(x, prefixIndex, 2, 4);
     }
 
     public static int calcPrefix(double x) {
+        if (x == 0) {
+            return 0;
+        }
         x = Math.abs(x);
-        double prefix = 1;
-        int prefixIndex = 0;
-        final double limit = 999.995;
-        if (x != 0) {
-            while (x / prefix < limit / 1000) {
-                prefix /= 1000;
-                prefixIndex--;
-            }
-        }
-        while (x / prefix >= limit) {
-            prefix *= 1000;
-            prefixIndex++;
-        }
-        return prefixIndex;
+        final int est = (int) Math.floor(Math.log10(x) / 3);
+        x *= Math.pow(1000, -est);
+        return (x >= 999.5) ? est + 1 : est;
     }
 
-    public static String engineeringDouble(double x, int prefixIndex) {
-        x *= Math.pow(1000, -prefixIndex);
-        return String.format("%7.2f %c", x, prefixIndex > 0 ? prefixes[prefixIndex] : negPrefixes[-prefixIndex]);
+    /**
+     * Convert a long to a string in engineering notation.
+     * <p/>
+     * This is meant to line things up in columns, so the output will be padded with spaces on the left if it is too short.
+     * <p/>
+     * In order to make it look nice when units are appended, the returned String will have a space at the end if the SI
+     * prefix is missing (prefix = 0) - for example "82 ". If the SI prefix exists, there will be a space between the
+     * number and the prefix - for example "82 M".
+     * <p/>
+     * Bug: Doesn't format Long.MIN_VALUE correctly. No plans to fix it.
+     *
+     * @param value       value to be converted
+     * @param prefixIndex index of SI prefix to display. -3=n, -2=u, -1=m, 0=no prefix, 1=k, 2=M, 3=G etc.
+     * @param nPost       Number of digits after the decimal place to display. If this is 0, no decimal place will be displayed.
+     * @param nPre        Minimum number of characters before the decimal place to display. Output will be left-padded up to this limit.
+     *                    digits and '-' count as characters before the decimal place.
+     * @return converted String
+     */
+    public static String formatDouble(double value, int prefixIndex, int nPost, int nPre) {
+        Require.geq(nPost, "nPost", 0);
+        Require.geq(nPre, "nPre", 0);
+        Require.finite(value);
+
+        final boolean negative = value < 0;
+        if (negative) {
+            value = -value;
+        }
+        // build it in reverse.
+        final StringBuilder sb = new StringBuilder();
+        if (prefixIndex > 0) {
+            sb.append(prefixes[prefixIndex]);
+        } else if (prefixIndex < 0) {
+            sb.append(negPrefixes[-prefixIndex]);
+        }
+
+        sb.append(' ');
+        int skip = prefixIndex * 3 - nPost;
+        value *= Math.pow(10, -skip);
+        long vRounded = Math.round(value);
+
+        appendShiftedLong(vRounded, sb, negative, nPre, nPost);
+
+        return sb.reverse().toString();
     }
+
+    /**
+     * Append a long to the StringBuilder, in reverse
+     *
+     * @param value    value to append
+     * @param sb       destination
+     * @param negative if true, insert a minus sign
+     * @param nPre     minimum number of characters before the decimal places (spaces added if needed to get it up to this level)
+     * @param nPost    number of decimals after the decimal place
+     */
+    private static void appendShiftedLong(long value, StringBuilder sb, boolean negative, int nPre, int nPost) {
+        for (int i = 0; i < nPost; i++) {
+            final char c = (char) ('0' + value % 10);
+            value /= 10;
+            sb.append(c);
+        }
+        if (nPost > 0) {
+            sb.append('.');
+        }
+        if (value == 0) {
+            sb.append('0');
+            nPre--;
+        } else {
+            while (value > 0) {
+                final char c = (char) ('0' + value % 10);
+                value /= 10;
+                sb.append(c);
+                nPre--;
+            }
+        }
+        if (negative) {
+            sb.append('-');
+            nPre--;
+        }
+        while (nPre-- > 0) {
+            sb.append(' ');
+        }
+    }
+
 
     ///////////////////////////////////////
     // Engineering format - long
@@ -69,7 +175,7 @@ public class Engineering {
      * @param value number to format
      * @return formatted string
      */
-    public static String formatLong(long value) {
+    public static String compactFormat(long value) {
         final AbscissaMantissa am = new AbscissaMantissa(value, 3);
         return am.toString();
     }
@@ -118,7 +224,7 @@ public class Engineering {
                 for (int i = 1; i < minSigDigit; i++) {
                     v0 /= 10;
                 }
-                v0 = (v0+5)/10;
+                v0 = (v0 + 5) / 10;
             }
 
             // See if the number of digits in the abscissa was changed by rounding
@@ -162,11 +268,18 @@ public class Engineering {
 
     }
 
+    /**
+     * Format a long as in {@link #formatLong(long, int, int, int)} with 2 digits after the decimal place and 4 characters before.
+     *
+     * @param value       value to format
+     * @param prefixIndex SI prefix: 0 = units, 1=k, 2=M, 3=G etc.
+     * @return formatted long
+     */
     public static String formatLong(long value, int prefixIndex) {
         for (int i = 0; i < prefixIndex; i++) {
             value /= 1000;
         }
-        return String.format("%,6d %c", value, prefixes[prefixIndex]);
+        return formatLong(value, prefixIndex, 2, 4);
     }
 
     /**
@@ -180,15 +293,15 @@ public class Engineering {
      * <p/>
      * Bug: Doesn't format Long.MIN_VALUE correctly. No plans to fix it.
      *
-     * @param value  value to be converted
-     * @param prefix index of SI prefix to display. 0=no prefix, 1=k, 2=M, 3=G etc.
-     * @param nPost  Number of digits after the decimal place to display. If this is 0, no decimal place will be displayed.
-     * @param nPre   Number of characters before the decimal place to display. Output will be left-padded up to this limit.
-     *               digits and '-' are characters to the left of the decimal place.
+     * @param value       value to be converted
+     * @param prefixIndex index of SI prefix to display. 0=no prefix, 1=k, 2=M, 3=G etc.
+     * @param nPost       Number of digits after the decimal place to display. If this is 0, no decimal place will be displayed.
+     * @param nPre        Minimum number of characters before the decimal place to display. Output will be left-padded up to this limit.
+     *                    digits and '-' count as characters before the decimal place.
      * @return converted String
      */
-    public static String formatLong(long value, int prefix, int nPost, int nPre) {
-        Require.geq(prefix, "prefix", 0);
+    public static String formatLong(long value, int prefixIndex, int nPost, int nPre) {
+        Require.geq(prefixIndex, "prefix", 0);
         Require.geq(nPost, "nPost", 0);
         Require.geq(nPre, "nPre", 0);
         Require.gt(value, "value", Long.MIN_VALUE);
@@ -197,17 +310,16 @@ public class Engineering {
         if (negative) {
             value = -value;
         }
-        final boolean hasDecimalPlace = nPost > 0;
 
         // build it in reverse.
         final StringBuilder sb = new StringBuilder();
-        if (prefix != 0) {
-            sb.append(prefixes[prefix]);
+        if (prefixIndex != 0) {
+            sb.append(prefixes[prefixIndex]);
         }
         sb.append(' ');
-        int skip = prefix * 3 - nPost;
+        int skip = prefixIndex * 3 - nPost;
         if (skip < 0) {
-            throw new IllegalArgumentException("Can't have " + nPost + " decimal places in a long with SI prefix " + prefixes[prefix]);
+            throw new IllegalArgumentException("Can't have " + nPost + " digits after the decimal place in a long with SI prefix " + prefixes[prefixIndex]);
         }
         while (skip-- > 1) {
             value /= 10;
@@ -221,32 +333,7 @@ public class Engineering {
             value /= 10;
         }
 
-        while (nPost-- > 0) {
-            final char c = (char) ('0' + value % 10);
-            value /= 10;
-            sb.append(c);
-        }
-        if (hasDecimalPlace) {
-            sb.append('.');
-        }
-        if (value == 0) {
-            sb.append('0');
-            nPre--;
-        } else {
-            while (value > 0) {
-                final char c = (char) ('0' + value % 10);
-                value /= 10;
-                sb.append(c);
-                nPre--;
-            }
-        }
-        if (negative) {
-            sb.append('-');
-            nPre--;
-        }
-        while (nPre-- > 0) {
-            sb.append(' ');
-        }
+        appendShiftedLong(value, sb, negative, nPre, nPost);
 
         return sb.reverse().toString();
     }
--- test/com/welty/othello/core/EngineeringTest.java
+++ test/com/welty/othello/core/EngineeringTest.java
@@ -9,90 +9,112 @@ public class EngineeringTest extends TestCase {
         assertEquals(1, Engineering.calculateNDigits(1));
         assertEquals(1, Engineering.calculateNDigits(0));
     }
-    public void testEngineeringWithLsp() {
-        testFormatLong(" 0 ", 0, 0, 0, 2);
-        testFormatLong("  0 ", 0, 0, 0, 3);
-        testFormatLong("  1 ", 1, 0, 0, 3);
-        testFormatLong(" -1 ", -1, 0, 0, 3);
-        testFormatLong("-1 ", -1, 0, 0, 1);
-        testFormatLong(" 10 ", 10, 0, 0, 3);
-        testFormatLong("0.01 k", 10, 1, 2, 1);
-        testFormatLong("0.01 k", 14, 1, 2, 1);
-        testFormatLong("0.02 k", 16, 1, 2, 1);
-        testFormatLong("-0.02 k", -16, 1, 2, 2);
-        testFormatLong(" 0.02 k", 16, 1, 2, 2);
-        testFormatLong("-0.02 k", -16, 1, 2, 1);
-        testFormatLong("12.3 k", 12345, 1, 1, 2);
+    public void testLongWithDetails() {
+        testLongWithDetails(" 0 ", 0, 0, 0, 2);
+        testLongWithDetails("  0 ", 0, 0, 0, 3);
+        testLongWithDetails("  1 ", 1, 0, 0, 3);
+        testLongWithDetails(" -1 ", -1, 0, 0, 3);
+        testLongWithDetails("-1 ", -1, 0, 0, 1);
+        testLongWithDetails(" 10 ", 10, 0, 0, 3);
+        testLongWithDetails("0.01 k", 10, 1, 2, 1);
+        testLongWithDetails("0.01 k", 14, 1, 2, 1);
+        testLongWithDetails("0.02 k", 16, 1, 2, 1);
+        testLongWithDetails("-0.02 k", -16, 1, 2, 2);
+        testLongWithDetails(" 0.02 k", 16, 1, 2, 2);
+        testLongWithDetails("-0.02 k", -16, 1, 2, 1);
+        testLongWithDetails("12.3 k", 12345, 1, 1, 2);
     }
 
-    private void testFormatLong(String expected, long value, int prefix, int nPost, int nPre) {
+    private void testLongWithDetails(String expected, long value, int prefix, int nPost, int nPre) {
         assertEquals(value+", prefix="+prefix+", nPost="+nPost + ", nPre=" + nPre, expected, Engineering.formatLong(value, prefix, nPost, nPre));
     }
 
-    public void testEngineering() {
-        testEngineeringLong("0 ", (long) 0);
-        testEngineeringLong("3 ", (long) 3);
-        testEngineeringLong("-3 ", (long) -3);
-        testEngineeringLong("12 ", (long) 12);
-        testEngineeringLong("123 ", (long) 123);
-        testEngineeringLong("1.23 k", (long) 1234);
-        testEngineeringLong("12.3 k", (long) 12345);
-        testEngineeringLong("99.9 k", (long) 99949);
+    public void testLongDefault() {
+        testLongDefault("0 ", (long) 0);
+        testLongDefault("3 ", (long) 3);
+        testLongDefault("-3 ", (long) -3);
+        testLongDefault("12 ", (long) 12);
+        testLongDefault("123 ", (long) 123);
+        testLongDefault("1.23 k", (long) 1234);
+        testLongDefault("12.3 k", (long) 12345);
+        testLongDefault("99.9 k", (long) 99949);
         // behaviour of 99500 intentionally unspecified - can round either way, we don't care.
-        testEngineeringLong("100 k", (long) 99951);
-        testEngineeringLong("100 k", (long) 99999);
-        testEngineeringLong("100 k", (long) 100000);
-        testEngineeringLong("123 k", (long) 123456);
-        testEngineeringLong("1.23 M", (long) 1234568);
-        testEngineeringLong("12.3 M", (long) 12345678);
-        testEngineeringLong("123 M", (long) 123456789);
+        testLongDefault("100 k", (long) 99951);
+        testLongDefault("100 k", (long) 99999);
+        testLongDefault("100 k", (long) 100000);
+        testLongDefault("123 k", (long) 123456);
+        testLongDefault("1.23 M", (long) 1234568);
+        testLongDefault("12.3 M", (long) 12345678);
+        testLongDefault("123 M", (long) 123456789);
 
         // Rounding
-        testEngineeringLong("99.9 M", (long) 99949999);
+        testLongDefault("99.9 M", (long) 99949999);
         // behaviour of 99500000 intentionally unspecified - can round either way, we don't care.
-        testEngineeringLong("100 M", (long) 99950001);
+        testLongDefault("100 M", (long) 99950001);
     }
 
 
-    private static void testEngineeringLong(String expected, long input) {
-        assertEquals(""+input, expected, Engineering.formatLong(input));
+    private static void testLongDefault(String expected, long input) {
+        assertEquals(""+input, expected, Engineering.compactFormat(input));
     }
 
-    public void testEngineeringDouble() {
-        testEngineeringDouble("   0.00  ", 0);
-        testEngineeringDouble("   3.00  ", 3);
-        testEngineeringDouble("  12.00  ", 12);
-        testEngineeringDouble(" 123.00  ", 123);
-        testEngineeringDouble("   1.23 k", 1234);
-        testEngineeringDouble("  12.34 k", 12344);
-        testEngineeringDouble("  99.99 k", 99990);
-        testEngineeringDouble(" 100.00 k", 100000);
-        testEngineeringDouble(" 123.45 k", 123450);
-        testEngineeringDouble(" 999.99 k", 999994);
-        testEngineeringDouble("   1.00 M", 999995);
-        testEngineeringDouble("   1.23 M", 1230000);
-        testEngineeringDouble("  12.34 M", 12340000);
-        testEngineeringDouble(" 123.45 M", 123450000);
+    public void testDouble() {
+        testDoubleDefault("0.00 ", 0);
+        testDoubleDefault("3.00 ", 3);
+        testDoubleDefault("12.0 ", 12);
+        testDoubleDefault("123 ", 123);
+        testDoubleDefault("1.23 k", 1234);
+        testDoubleDefault("12.3 k", 12344);
+        testDoubleDefault("99.9 k", 99900);
+        testDoubleDefault("100 k", 100000);
+        testDoubleDefault("123 k", 123450);
+        testDoubleDefault("999 k", 999400);
+        testDoubleDefault("1.00 M", 999500);
+        testDoubleDefault("1.23 M", 1230000);
+        testDoubleDefault("12.3 M", 12340000);
+        testDoubleDefault("123 M", 123450000);
 
-        testEngineeringDouble(" 999.99 m", 0.99999);
-        testEngineeringDouble(" 100.00 m", 0.1);
-        testEngineeringDouble(" 100.00 m", 0.099995);
-        testEngineeringDouble("  99.99 m", 0.099994);
-        testEngineeringDouble("  10.00 m", 0.01);
-        testEngineeringDouble("  10.00 m", 0.009995);
-        testEngineeringDouble("   9.99 m", 0.009994);
-        testEngineeringDouble("   1.00 m", 0.001);
-        testEngineeringDouble("   1.00 m", 0.000999995);
-        testEngineeringDouble(" 999.99 μ", 0.000999994);
-        testEngineeringDouble("   1.00 μ", 0.000001);
+        testDoubleDefault("1.00 ", 0.99999);
+        testDoubleDefault("100 m", 0.1);
+        testDoubleDefault("100 m", 0.09996);
+        testDoubleDefault("99.9 m", 0.09994);
+        testDoubleDefault("10.0 m", 0.01);
+        testDoubleDefault("10.0 m", 0.009995);
+        testDoubleDefault("9.99 m", 0.009994);
+        testDoubleDefault("1.00 m", 0.001);
+        testDoubleDefault("1.00 m", 0.0009996);
+        testDoubleDefault("999 μ", 0.0009994);
+        testDoubleDefault("1.00 μ", 0.000001);
 
-        testEngineeringDouble("-999.99 m", -0.99999);
+        testDoubleDefault("-999 m", -0.999);
 
     }
 
 
-    private static void testEngineeringDouble(String expected, double input) {
-        assertEquals(expected, Engineering.engineeringDouble(input));
+    private static void testDoubleDefault(String expected, double input) {
+        assertEquals(expected, Engineering.compactFormat(input));
     }
 
+    public void testDoubleWithDetails() {
+        testDoubleWithDetails(" 0.00 ", 0., 0, 2, 2);
+        testDoubleWithDetails(" 0 ", 0., 0, 0, 2);
+        testDoubleWithDetails("  0 ", 0., 0, 0, 3);
+        testDoubleWithDetails("1.23 m", .001234, -1, 2, 0);
+        testDoubleWithDetails(" 1.00 ", 1., 0, 2, 2);
+        testDoubleWithDetails("  1 ", 1., 0, 0, 3);
+        testDoubleWithDetails(" -1 ", -1., 0, 0, 3);
+        testDoubleWithDetails("-1 ", -1., 0, 0, 1);
+        testDoubleWithDetails(" 10 ", 10., 0, 0, 3);
+        testDoubleWithDetails("0.01 k", 10., 1, 2, 1);
+        testDoubleWithDetails("0.01 k", 14., 1, 2, 1);
+        testDoubleWithDetails("0.02 k", 16., 1, 2, 1);
+        testDoubleWithDetails("-0.02 k", -16., 1, 2, 2);
+        testDoubleWithDetails(" 0.02 k", 16., 1, 2, 2);
+        testDoubleWithDetails("-0.02 k", -16., 1, 2, 1);
+        testDoubleWithDetails("12.3 k", 12345., 1, 1, 2);
+    }
+
+    private void testDoubleWithDetails(String expected, double value, int prefix, int nPost, int nPre) {
+        assertEquals(value + ", prefix=" + prefix + ", nPost=" + nPost + ", nPre=" + nPre, expected, Engineering.formatDouble(value, prefix, nPost, nPre));
+    }
 }
